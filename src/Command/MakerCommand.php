740cacfee10be3b19394408f14622b66460e9544
Luigi Massa <lmassa@bwlab.it>
2021-12-16 22:16:46 +0100

    aggunta dei test

--- src/Command/MakerCommand.php
+++ src/Command/MakerCommand.php
@@ -14,218 +14,158 @@ class MakerCommand extends Command
 {
     protected Filesystem $filesystem;
     protected string $base_controller_folder;
+    protected string $base_test_folder;
     protected string $base_folder;
     private Environment $twig;
-    
+
     public function __construct(Environment $environment)
     {
-        parent::__construct('bwlab:module:setup');
+        parent::__construct( 'bwlab:module:setup' );
         $this->twig = $environment;
         $this->filesystem = new Filesystem();
         $this->base_folder = _PS_MODULE_DIR_.'bwmodulesetup/views/templates/module';
         $this->base_controller_folder = $this->base_folder.'/controller';
+        $this->base_test_folder = $this->base_folder.'/test';
     }
-    
+
     protected function configure(): void
     {
         // The name of the command (the part after "bin/console")
-        $this->setName('bwlab:module:setup')
-            ->addArgument('modulename', InputArgument::REQUIRED)
-            ->addArgument('namespace', InputArgument::REQUIRED);
+        $this->setName( 'bwlab:module:setup' )->addArgument( 'modulename', InputArgument::REQUIRED )
+             ->addArgument( 'namespace', InputArgument::REQUIRED );
     }
-    
+
     protected function createControllerTemplate($modulename, $templatename)
     {
-        
-        $module_view_path = _PS_MODULE_DIR_
-            .$modulename.DIRECTORY_SEPARATOR
-            .'views'
-            .DIRECTORY_SEPARATOR
-            .'templates'.DIRECTORY_SEPARATOR
-            .'admin'.DIRECTORY_SEPARATOR
-            .'controller';
-        
-        $this->filesystem->mkdir(
-            $module_view_path
-        );
-        $this->filesystem->copy(
-            $this->base_controller_folder.DIRECTORY_SEPARATOR.'template_controller.twig',
-            $module_view_path
-            .DIRECTORY_SEPARATOR.'admin_configuration.html.twig'
-        );
-        
+
+        $module_view_path =
+            $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'templates'.
+            DIRECTORY_SEPARATOR.'admin'.DIRECTORY_SEPARATOR.'controller';
+        $this->filesystem->mkdir( $module_view_path );
+        $this->filesystem->copy( $this->base_controller_folder.DIRECTORY_SEPARATOR.'template_controller.twig',
+            $module_view_path.DIRECTORY_SEPARATOR.'admin_configuration.html.twig' );
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $modulename = $input->getArgument('modulename');
-        $namespace = $input->getArgument('namespace');
-    
-        $output->writeln('create module folder');
-        $this->createModule($modulename);
-        $output->writeln('create main file');
-        $this->createMain($modulename, $namespace);
-        $output->writeln('create composer.json');
-        $this->creteComposerJson($modulename, $namespace);
-        $output->writeln('create config');
-        $this->createConfig($modulename);
-        $output->writeln('create routes');
-        $this->createRoute($modulename, $namespace);
-        $output->writeln('create configuration controller');
-        $this->createController($modulename, $namespace);
-        $output->writeln('create form ');
-        $this->createControllerForm($modulename, $namespace);
-        $output->writeln('create configuration controller template');
-        $this->createControllerTemplate($modulename, $namespace);
-        $output->writeln('....');
-        $output->writeln('OK! Now you can run "composer install" inside the module.');
-        $output->writeln('');
+        $modulename = $input->getArgument( 'modulename' );
+        $namespace = $input->getArgument( 'namespace' );
+        $output->writeln( 'create module folder' );
+        $this->createModule( $modulename );
+        $output->writeln( 'create main file' );
+        $this->createMain( $modulename, $namespace );
+        $output->writeln( 'create composer.json' );
+        $this->creteComposerJson( $modulename, $namespace );
+        $output->writeln( 'create config' );
+        $this->createConfig( $modulename );
+        $output->writeln( 'create routes' );
+        $this->createRoute( $modulename, $namespace );
+        $output->writeln( 'create configuration controller' );
+        $this->createController( $modulename, $namespace );
+        $output->writeln( 'create form ' );
+        $this->createControllerForm( $modulename, $namespace );
+        $output->writeln( 'create configuration controller template' );
+        $this->createControllerTemplate( $modulename, $namespace );
+        $output->writeln( 'create test folder' );
+        $this->createTest( $modulename );
+        $output->writeln( '....' );
+        $output->writeln( 'OK! Now you can run "composer install" inside the module.' );
+        $output->writeln( '' );
     }
-    
+
     private function createConfig($modulename)
     {
-        $module_config_path = _PS_MODULE_DIR_
-            .$modulename.DIRECTORY_SEPARATOR
-            .'config'
-            .DIRECTORY_SEPARATOR
-            .'admin';
-        $this->filesystem->mkdir(
-            $module_config_path
-        );
-        $this->filesystem->copy(
-            $this->base_controller_folder.DIRECTORY_SEPARATOR.'services.yml',
-            $module_config_path
-            .DIRECTORY_SEPARATOR.'services.yml'
-        );
-        
+        $module_config_path =
+            $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'admin';
+        $this->filesystem->mkdir( $module_config_path );
+        $this->filesystem->copy( $this->base_controller_folder.DIRECTORY_SEPARATOR.'services.yml',
+            $module_config_path.DIRECTORY_SEPARATOR.'services.yml' );
     }
-    
+
     private function createController($modulename, $namespace)
     {
-        
-        $controller_code = $this->twig->render(
-            $this->base_controller_folder.DIRECTORY_SEPARATOR.'configuration.php.twig',
-            [
-                'class_name' => 'ConfigurationController',
-                'module_name' => $modulename,
-                'name_space' => $namespace,
-            ]
-        );
-        
-        $file = PhpFile::fromCode($controller_code);
-        
-        
-        $this->filesystem->dumpFile(
-            _PS_MODULE_DIR_
-            .$modulename.DIRECTORY_SEPARATOR
-            .'src'
-            .DIRECTORY_SEPARATOR
-            .'Controller'
-            .DIRECTORY_SEPARATOR.'ConfigurationController.php',
-            $file->__toString()
-        );
-        
+
+        $controller_code =
+            $this->twig->render( $this->base_controller_folder.DIRECTORY_SEPARATOR.'configuration.php.twig', [
+                    'class_name' => 'ConfigurationController',
+                    'module_name' => $modulename,
+                    'name_space' => $namespace,
+                ] );
+        $file = PhpFile::fromCode( $controller_code );
+        $this->filesystem->dumpFile( $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.'src'.
+            DIRECTORY_SEPARATOR.'Controller'.DIRECTORY_SEPARATOR.'ConfigurationController.php', $file->__toString() );
     }
-    
+
     private function createControllerForm($modulename, $namespace)
     {
-        $controller_code = $this->twig->render(
-            $this->base_controller_folder.DIRECTORY_SEPARATOR.'form.php.twig',
-            [
+        $controller_code = $this->twig->render( $this->base_controller_folder.DIRECTORY_SEPARATOR.'form.php.twig', [
                 'class_name' => 'ConfigurationType',
                 'module_name' => $modulename,
                 'name_space' => $namespace,
-            ]
-        );
-    
-        $file = PhpFile::fromCode($controller_code);
-    
-    
-        $this->filesystem->dumpFile(
-            _PS_MODULE_DIR_
-            .$modulename.DIRECTORY_SEPARATOR
-            .'src'
-            .DIRECTORY_SEPARATOR
-            .'Form'
-            .DIRECTORY_SEPARATOR
-            .'Type'
-            .DIRECTORY_SEPARATOR
-            .'ConfigurationType.php',
-            $file->__toString()
-        );
+            ] );
+        $file = PhpFile::fromCode( $controller_code );
+        $this->filesystem->dumpFile( $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.'src'.
+            DIRECTORY_SEPARATOR.'Form'.DIRECTORY_SEPARATOR.'Type'.DIRECTORY_SEPARATOR.'ConfigurationType.php',
+            $file->__toString() );
     }
-    
+
     private function createMain($modulename, $namespace)
     {
-        
-        $controller_code = $this->twig->render(
-            $this->base_folder.DIRECTORY_SEPARATOR.'main.php.twig',
-            [
+
+        $controller_code = $this->twig->render( $this->base_folder.DIRECTORY_SEPARATOR.'main.php.twig', [
                 'module_name' => $modulename,
-            ]
-        );
-        
-        $file = PhpFile::fromCode($controller_code);
-        
-        
-        $this->filesystem->dumpFile(
-            _PS_MODULE_DIR_
-            .$modulename
-            .DIRECTORY_SEPARATOR.$modulename.'.php',
-            $file->__toString()
-        );
-        
+            ] );
+        $file = PhpFile::fromCode( $controller_code );
+        $this->filesystem->dumpFile( $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.$modulename.'.php',
+            $file->__toString() );
     }
-    
+
     private function createModule($modulename)
     {
-        $this->filesystem->mkdir(_PS_MODULE_DIR_.$modulename);
-        
+        $this->filesystem->mkdir( $this->getModuleDirectory( $modulename ) );
     }
-    
+
     private function createRoute($modulename, $namespace)
     {
-        $module_route_path = _PS_MODULE_DIR_
-            .$modulename.DIRECTORY_SEPARATOR
-            .'config';
-        if ($this->filesystem->exists($module_route_path) === false) {
-            $this->filesystem->mkdir(
-                $module_route_path
-            );
+        $module_route_path = $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.'config';
+        if ($this->filesystem->exists( $module_route_path ) === false) {
+            $this->filesystem->mkdir( $module_route_path );
         }
-        $route_code = $this->twig->render(
-            $this->base_controller_folder.DIRECTORY_SEPARATOR.'routes.yml.twig',
-            [
+        $route_code = $this->twig->render( $this->base_controller_folder.DIRECTORY_SEPARATOR.'routes.yml.twig', [
                 'module_name' => $modulename,
                 'name_space' => $namespace,
-            ]
-        );
-        
-        
-        $this->filesystem->dumpFile(
-            $module_route_path
-            .DIRECTORY_SEPARATOR.'routes.yml',
-            $route_code
-        );
-        
+            ] );
+        $this->filesystem->dumpFile( $module_route_path.DIRECTORY_SEPARATOR.'routes.yml', $route_code );
     }
-    
+
+    private function createTest($modulename)
+    {
+        $module_dir = $this->getModuleDirectory( $modulename );
+        $test_dir = $module_dir.DIRECTORY_SEPARATOR.'test';
+        $this->filesystem->mkdir( $test_dir );
+        $this->filesystem->copy( $this->base_test_folder.DIRECTORY_SEPARATOR.'bootstrap.php.twig',
+            $test_dir.DIRECTORY_SEPARATOR.'bootstrap.php' );
+        $this->filesystem->copy( $this->base_test_folder.DIRECTORY_SEPARATOR.'phpunit.xml.twig',
+            $module_dir.DIRECTORY_SEPARATOR.'phpunit.xml' );
+    }
+
     private function creteComposerJson($modulename, $namespace)
     {
-        $composer_code = $this->twig->render(
-            $this->base_folder.DIRECTORY_SEPARATOR.'composer.json.twig',
-            [
+        $composer_code = $this->twig->render( $this->base_folder.DIRECTORY_SEPARATOR.'composer.json.twig', [
                 'module_name' => $modulename,
-                'name_space_psr4' => str_replace('\\', '\\\\', $namespace),
-            ]
-        );
-        
-        $this->filesystem->dumpFile(
-            _PS_MODULE_DIR_
-            .$modulename
-            .DIRECTORY_SEPARATOR.'composer.json',
-            $composer_code
-        );
+                'name_space_psr4' => str_replace( '\\', '\\\\', $namespace ),
+            ] );
+        $this->filesystem->dumpFile( $this->getModuleDirectory( $modulename ).DIRECTORY_SEPARATOR.'composer.json',
+            $composer_code );
     }
-    
+
+    /**
+     * @param $modulename
+     * @return string
+     */
+    private function getModuleDirectory($modulename): string
+    {
+        return _PS_MODULE_DIR_.$modulename;
+    }
+
 }
